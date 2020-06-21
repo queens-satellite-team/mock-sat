8eab746cdf4e53931a04ee1f58ef2abda8e6eb55
Cal <cal.graham707@gmail.com>
2020-06-21 17:24:44 -0400

    Changed output pins to mirro PID_TEST.py script

--- ADCS_Prototyping/FIT0441_TEST.py
+++ ADCS_Prototyping/FIT0441_TEST.py
@@ -4,14 +4,16 @@ IO.setmode(IO.BCM)
 
 IO.setwarnings(False)
 
-IO.setup(26, IO.OUT)
-p = IO.PWM(26, 35000)
+IO.setup(18, IO.OUT)
+p = IO.PWM(18, 100)
 
-IO.setup(3, IO.OUT)
-IO.output(3, IO.HIGH)
+IO.setup(15, IO.OUT)
+IO.output(15, IO.HIGH)
 
-p.start(90)
-time.sleep(1)
+p.start(100)
+for x in range(100):
+	time.sleep(0.1)
+	p.ChangeDutyCycle(x)
+
 p.stop()
-
 IO.cleanup()
