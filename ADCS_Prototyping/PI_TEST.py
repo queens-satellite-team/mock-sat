73f6b52cfca69930f2f6613d548babe2d8815373
Cal <cal.graham707@gmail.com>
2020-07-11 14:31:39 -0400

    Semi function control loop iteration, PWM control range of FIT0441 motor and accuracy of BNO055 on single axis need to be assessed - tested with string support

--- ADCS_Prototyping/PI_TEST.py
+++ ADCS_Prototyping/PI_TEST.py
@@ -13,7 +13,7 @@ except:
 	print('Error initializing BNO055 sensor')
 
 print('BNO055 sensor reading initialized')
-time.sleep(1)
+time.sleep(0.1)
 
 #set control pins on pi - note that 18 is the only hardware PWM pin on 3B+ model
 PWM_CTRL = 18
@@ -37,7 +37,7 @@ except:
 	print('Error initializing FIT0441 control signal')
 
 print('FIT0441 control initialized')
-time.sleep(1)
+time.sleep(0.1)
 
 DES_EUL = int(input('Enter desired orientation (between 0 and 360):'))
 print('Desired angle is: {}' .format(DES_EUL))
@@ -59,50 +59,65 @@ def vel(): #returns angular velocity in rads/sec
 		print('ERROR IN VELOCITY READ')
 		VEL = vel()
 	return VEL
-"""
-def detumble(): #could be changed simply to apply magnetorquers instead of wheels
-        VEL = vel()
-        if VEL > 0:
-                IO.output(DIR_CTRL, IO.HIGH)
-        else:
-                IO.output(DIR_CTRL, IO.LOW)
 
-        M.ChangeDutyCycle(
-"""
-for x in range(20):
-VEL = vel()
-#        if VEL > VEL_TOL:
+#def detumble(): #could be changed simply to apply magnetorquers instead of wheels
+#        VEL = vel()
+#        if VEL > 0:
+#                IO.output(DIR_CTRL, IO.HIGH)
+#        else:
+#                IO.output(DIR_CTRL, IO.LOW)
+#
+#        M.ChangeDutyCycle(
+
+for x in range(500):
+	VEL = vel()
+#        if VEL > TOL_VEL:
 #               detumble()
 
 	EUL = eul()
 	DIFF = DES_EUL - EUL
-	if DIFF < EUL_TOL:
+	if abs(DIFF) < TOL_EUL:
 		time.sleep(0.01)
 		continue
 
 	if abs(DIFF) > 270:
 		DIFF = (-1)*abs(abs(DIFF) - 360)*DIFF/abs(DIFF)
 
-	print('P control DIFF')
+#	print('P control DIFF')
+#	print(DIFF/2)
+
+	DIFF = DIFF/3 + (VEL*180/3.14)/6
+
+#	print('PI control DIFF')
+#	print(DIFF)
+
+	#define direction
+	if DIFF > 0:
+		IO.output(DIR_CTRL, IO.LOW)
+	elif DIFF < 0:
+		IO.output(DIR_CTRL, IO.HIGH)
+
+	DUTY = 100 - abs(DIFF) #converty to duty percentage
+
+#	if abs(DUTY - DUTY_PREV) > 10: #max acc
+#		DUTY = DUTY_PREV + 10*(DUTY_PREV - DUTY)/abs(DUTY_PREV - DUTY)
+
+#	if abs(DUTY - DUTY_PREV) < 2 and DUTY != 100 and DUTY != DUTY_PREV: #min acc
+#		DUTY = DUTY_PREV + 2*(DUTY_PREV - DUTY)/abs(DUTY_PREV - DUTY)
+
+	if DUTY < 10: #max vel
+		DUTY = 10
+	elif DUTY > 100: #no vel
+		DUTY = 100
+
+	DUTY_PREV = DUTY
+
+	print(EUL)
 	print(DIFF)
-	time.sleep(1)
+	print(DUTY)
 
-	DIFF = DIFF + VEL*0.01*180/3.14
-
-	print('PI control DIFF')
-	print(DIFF)
-	time.sleep(1)
-
-"""
-        if DIFF > 0:
-            IO.output(DIR_CTRL, IO.HIGH)
-        elif DIFF < 0:
-            IO.outptu(DIR_CTRL, IO.LOW)
-
-        DUTY = 100 - abs(
-"""
-
-	time.sleep(1)
+	M.ChangeDutyCycle(int(DUTY))
+	time.sleep(0.01)
 
 M.stop()
 IO.cleanup()
