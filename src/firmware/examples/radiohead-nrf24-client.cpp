67af00ffbda02be6ab1d795d1a40110b88d61ed9
Anthony Steel <13as261@queensu.ca>
2020-07-15 12:39:17 -0400

    Move example files to be in firmware directory

---
+++ src/firmware/examples/radiohead-nrf24-client.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <RH_NRF24.h>
+
+RH_NRF24 nrf24;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  if (!nrf24.init()) Serial.println("init failed");
+  if (!nrf24.setChannel(1)) Serial.println("setChannel failed");
+  if (!nrf24.setRF(NH_RF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
+    Serial.println("setRF failed");
+}
+
+void loop() {
+  Serial.println("Sending to nrf24_server");
+  uint8_t data[] = "Hello World!";
+  nrf24.send(data, sizeof(data));
+
+  nrf24.waitPacketSent();
+  uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  if (nrf24.waitAvailableTimeout(500)) {
+    if (nrf24.recv(buf, &len)) {
+      Serial.print("got reply: ");
+      Serial.println((char *)buf);
+    } else {
+      Serial.println("recv failed");
+    }
+  } else {
+    Serial.println("No reply, is nrf24_server running?");
+  }
+  delay(400);
+}
