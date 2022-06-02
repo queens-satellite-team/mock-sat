759db6dc19224e18ada3c9000c11d39fbc96399e
Sean Tedesco <stedesco@live.com>
2022-06-01 23:23:34 -0400

    comms: rename dir from comms_msat to comms

---
+++ src/comms/radio.py
@@ -0,0 +1,9 @@
+class Radio:
+    def __init__(self):
+        print('called radio init!')
+
+    def transmit(self):
+        print('radio is transmitting!')
+
+    def receive(self):
+        print('radio is receiving!')
