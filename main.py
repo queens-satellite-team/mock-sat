2e4acd36f36bf4554121371ee7088908b755ab67
Sean Tedesco <stedesco@live.com>
2022-06-01 23:40:59 -0400

    main: include example imports

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import imp
+from src.comms.radio import Radio
+from src.payload.img_compr_raspi import PiCam
+
 def print_header():
     print('--------------------------------')
     print('\tQSAT Mock-Sat Program')
@@ -10,7 +14,13 @@ def print_footer():
 
 def main():
     print_header()
-    
+
+    comms = Radio()
+    comms.transmit()
+    comms.receive()
+
+    payload = PiCam()
+
     print_footer()
 
 if __name__ == '__main__':
