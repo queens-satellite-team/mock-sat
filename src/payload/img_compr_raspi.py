be1288a71b8e56409cb6397417e9a90042b9ce58
Sean Tedesco <stedesco@live.com>
2022-06-01 23:40:24 -0400

    payload: make picam a seperate class

--- src/payload/img_compr_raspi.py
+++ src/payload/img_compr_raspi.py
@@ -2,41 +2,58 @@
 
 from picamera import PiCamera
 from time import sleep
+from datetime import datetime
 from PIL import Image
 from matplotlib import image
 from matplotlib import pyplot as plt
 import os
 
-camera = PiCamera()
-image_path = "/home/pi/Desktop/image.jpg"
-compressed_image_path = "/home/pi/Desktop/image_compressed.jpg"
+class PiCam(PiCamera):
+    
+    def __init__(self):
+        super().__init__()
+        print('called PiCam init!')
 
-# Take the picture
-camera.start_preview()
-sleep(5)  # camera warm-up time
-camera.capture(image_path)
-camera.stop_preview()
-camera.close()
+    def shot(self, filename:str='image.jpg'):
+        self.start_preview()
+        sleep(2)
+        self.capture(filename)
+        self.stop_preview()
+        self.close()
 
-# Compress the image
-img = Image.open(image_path)
-width, height = img.size
-scale_factor = 10
-img_compressed = img.resize((int(width / scale_factor), int(height / scale_factor)))
-img_compressed.save(compressed_image_path)
-img_size = os.path.getsize(image_path)
-img_compressed_size = os.path.getsize(compressed_image_path)
 
-# Plot
-plt.figure(figsize=(15,12))
+def main():
 
-plt.subplot(121)
-plt.imshow(img)
-#plt.title(f"Original Image ({(img_size / 1024):.2f} kB)")
+    # set path names to be used in the main call
+    image_path = "/home/pi/Desktop/image.jpg"
+    compressed_image_path = "/home/pi/Desktop/image_compressed.jpg"
+    
+    # create a picam object and make it take a photo
+    cam = PiCam()
+    cam.shot(image_path)
 
-plt.subplot(122)
-plt.imshow(img_compressed)
-#plt.title(f"Compressed Image ({(img_compressed_size / 1024):.2f} kB)")
+    # Compress the image
+    img = Image.open(image_path)
+    width, height = img.size
+    scale_factor = 10
+    img_compressed = img.resize((int(width / scale_factor), int(height / scale_factor)))
+    img_compressed.save(compressed_image_path)
+    img_size = os.path.getsize(image_path)
+    img_compressed_size = os.path.getsize(compressed_image_path)
 
-plt.show()
-plt.savefig("/home/pi/Desktop/compression_results.png")
+    # Plot
+    plt.figure(figsize=(15,12))
+
+    plt.subplot(121)
+    plt.imshow(img)
+    #plt.title(f"Original Image ({(img_size / 1024):.2f} kB)")
+
+    plt.subplot(122)
+    plt.imshow(img_compressed)
+    #plt.title(f"Compressed Image ({(img_compressed_size / 1024):.2f} kB)")
+
+    plt.show()
+    plt.savefig("/home/pi/Desktop/compression_results.png")
+
+if __name__ == '__main__':
+    main()
