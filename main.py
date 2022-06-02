f28a48233280eb3a9ce4e7550fe691090cff4b33
Sean Tedesco <stedesco@live.com>
2022-06-01 23:12:04 -0400

    main: create main entry point for mock-sat program

---
+++ main.py
@@ -0,0 +1,17 @@
+def print_header():
+    print('--------------------------------')
+    print('\tQSAT Mock-Sat Program')
+    print('--------------------------------')
+    print()
+
+def print_footer():
+    print('--------------------------------')
+    print('--------------------------------')
+
+def main():
+    print_header()
+    
+    print_footer()
+
+if __name__ == '__main__':
+    main()
