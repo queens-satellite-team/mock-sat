702da8c476acba721c02121ff69afd1923b0d3bf
SeanTedesco <stedesco@live.com>
2022-06-04 11:38:48 -0400

    radio: inherent base Arduino class

--- src/comms/radio.py
+++ src/comms/radio.py
@@ -1,5 +1,8 @@
-class Radio:
+from common.arduino import Arduino
+
+class Radio(Arduino):
     def __init__(self):
+        super().__init__()
         print('called radio init!')
 
     def transmit(self):
