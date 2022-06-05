74ed665ea1f349b6bfd284036f371ac92ed756c1
Sean Tedesco <stedesco@live.com>
2022-06-05 16:26:19 -0400

    common: arduino: support generic i2c communication

--- src/common/arduino.py
+++ src/common/arduino.py
@@ -11,6 +11,8 @@ to it from: mock-sat/firmware/common/arduino/arduino.ino
 
 import serial
 from serial import SerialTimeoutException
+import smbus
+import time
 
 class Arduino:
     """Arduino base class. This class can be inherited by a parent
@@ -22,6 +24,9 @@ class Arduino:
     def __init__(self, port='/dev/ttyACM0', baud=115200, timeout=1) -> None:
         self.ser = serial.Serial(port=port, baudrate=baud, timeout=timeout)
         self.ser.reset_input_buffer()
+
+        self.bus = smbus.SMBus(1)
+        self.i2c_address = 0x04
 
     def send_over_serial(self, string:str='hello world'):
         ''' Send a message with serial (UART) communication.
@@ -62,11 +67,47 @@ class Arduino:
             line = self.ser.readline().decode('utf-8').rstrip()
         return line
 
-    def send_over_i2c(self):
-        pass
+    def send_over_i2c(self, string:str='hello world'):
+        '''Send a message using the I2C bus communication.
+
+        Params:
+            - string: the data to send.
+
+        Returns:
+            - count (int): the number of bytes written.
+
+        Raises:
+            - None
+        '''
+        count = 0
+        for char in string:
+            data = int(ord(char))
+            try:
+                self.bus.write_byte(self.address, data)
+            except Exception as e:
+                print(f'{self} could not send message {char} over i2c.')
+                raise e
+            count += 1
+        return count
 
     def receive_over_i2c(self):
-        pass
+        '''Receive a message using the I2C bus communication.
+
+        Params:
+            - None
+
+        Returns:
+            - count (int): the number of bytes written.
+
+        Raises:
+            - None
+
+        TODO:
+            - include start and stop characters to receive more
+            that one value at a time!
+        '''
+        number = self.bus.read_byte_data(self.i2c_address, 1)
+        return number
 
     def send_over_spi(self):
         pass
