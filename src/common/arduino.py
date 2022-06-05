604df52f3f487fdfa363dd01ac37bbf0b2662856
SeanTedesco <stedesco@live.com>
2022-06-04 20:16:26 -0400

    common: arduino: support serial communication

--- src/common/arduino.py
+++ src/common/arduino.py
@@ -9,6 +9,9 @@ the matching Arduino is expected to have the firmware file loaded on
 to it from: mock-sat/firmware/common/arduino/arduino.ino
 """
 
+import serial
+from serial import SerialTimeoutException
+
 class Arduino:
     """Arduino base class. This class can be inherited by a parent
     classes, i.e. one of the sub-system devices (radio, camera,
@@ -16,23 +19,57 @@ class Arduino:
     will have access to all methods defined here.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, port='/dev/ttyACM0', baud=115200, timeout=1) -> None:
+        self.ser = serial.Serial(port=port, baudrate=baud, timeout=timeout)
+        self.ser.reset_input_buffer()
+
+    def send_over_serial(self, string:str='hello world'):
+        ''' Send a message with serial (UART) communication.
+
+        Params:
+            - string: the data to send.
+
+        Returns:
+            - count (int): the number of bytes written.
+
+        Raises:	
+            - SerialTimeoutException: In case a write timeout is configured for the port and the time is exceeded.
+        '''
+        self.ser.flushOutput()
+        count = 0
+        try:
+            count = self.ser.write(string.encode('utf-8'))
+        except SerialTimeoutException as e:
+            print(f'{self} failed to write over serial. Is the Arduino plugged in?')
+            raise e
+        return count
+
+    def receive_over_serial(self):
+        '''Read and return one line from the USB port.
+
+        Params:
+            - None
+        
+        Returns:
+            - line (string): the data received from the USB port
+                or "xxx" if no data is available.
+
+        Raises:
+            - None
+        '''
+        line = 'xxx'
+        if self.ser.inWaiting() > 0:
+            line = self.ser.readline().decode('utf-8').rstrip()
+        return line
+
+    def send_over_i2c(self):
         pass
 
-    def send_over_serial(s):
+    def receive_over_i2c(self):
         pass
 
-    def receive_over_serial():
+    def send_over_spi(self):
         pass
 
-    def send_over_i2c():
-        pass
-
-    def receive_over_i2c():
-        pass
-
-    def send_over_spi():
-        pass
-
-    def receive_over_spi():
+    def receive_over_spi(self):
         pass
