94510e006a07f34566ca57960e4d156b0275ea0f
SeanTedesco <stedesco@live.com>
2022-06-08 08:14:11 -0400

    main: import tasks

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
-import imp
+import importlib
+from src.satellite import mock_sat
 from src.comms.radio import Radio
+from colorama import Fore
+import os
 
 def print_header():
-    print('--------------------------------')
-    print('\tQSAT Mock-Sat Program')
-    print('--------------------------------')
+    print(Fore.RED + '-------------------------------------' + Fore.WHITE, flush=True)
+    print(Fore.CYAN + '\tQSAT Mock-Sat Program', flush=True)
+    print(Fore.RED + '-------------------------------------' + Fore.WHITE, flush=True)
     print()
 
 def print_footer():
     print()
-    print('--------------------------------')
-    print('--------------------------------')
+    print(Fore.RED + '-------------------------------------' + Fore.WHITE, flush=True)
+    print(Fore.RED + '-------------------------------------' + Fore.WHITE, flush=True)
 
 def main():
     print_header()
+
+    print('['+Fore.GREEN+'000000'+Fore.WHITE+'] Loading Tasks... ')
+    # schedule all tasks in directory
+    for file in os.listdir('tasks'):
+        # remove the '.py' from file name
+        file=file[:-3]
+
+        # ignore these files
+        if file in ('template_task') or file.startswith('_'):
+            continue
+
+        # auto-magically import the task file
+        exec('import tasks.{}'.format(file))
+        print('{}{}'.format('\t└── ',file))
+
+        # create a helper object for scheduling the task
+        task_obj=eval('tasks.'+file).task(mock_sat)
+    
+
+    print('['+Fore.GREEN+'000001'+Fore.WHITE+'] Running Tasks... ')
+    print()
 
     comms = Radio()
     comms.transmit()
